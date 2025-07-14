b16a56eef25f2358bff022a1730408eb9015d175
4n4k1n <anakin@pregitzer.de>
2025-07-14 15:41:47 +0200

    function to check if the args are valid

---
+++ philo/src/valid_input.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 12:31:35 by anakin            #+#    #+#             */
+/*   Updated: 2025/07/14 12:38:48 by anakin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+
+static int check_arg(char *arg)
+{
+	int i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] != ' ' || arg[i] != '-' || arg[i] != '+' || !(arg[i] > '0' && arg[i] < '9'))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int valid_input(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while(i < ac)
+	{
+		if (check_arg(av[i]) == 1)
+			return (1);
+		i++;
+	}
+	return (0);
+}
