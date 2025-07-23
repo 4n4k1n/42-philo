b03243c97720f6eb61bba32e7e7e77e889cb992d
4n4k1n <anakin@pregitzer.de>
2025-07-23 13:16:07 +0200

    added enum for philo state and added new function prototypes

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 10:58:21 by anakin            #+#    #+#             */
-/*   Updated: 2025/07/14 12:53:00 by anakin           ###   ########.fr       */
+/*   Updated: 2025/07/19 09:50:06 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 # include <stdlib.h>
 # include <string.h>
 
+typedef enum e_state
+{
+	THINK,
+	EAT,
+	TRY_EAT,
+	SLEEP,
+	DEAD
+}			t_state;
+
+typedef struct s_philo	t_philo;
 
 typedef struct s_data
 {
@@ -25,6 +35,7 @@ typedef struct s_data
 	int	tt_die;
 	int	tt_sleep;
 	int	meals_to_eat;
+	t_philo	*philos;
 }		t_data;
 
 typedef struct	s_philo
@@ -33,14 +44,18 @@ typedef struct	s_philo
 	pthread_t		thread;
 	size_t			id;
 	pthread_mutex_t	left_fork;
-	pthread_mutex_t	right_fork;
+	pthread_mutex_t	*right_fork;
 	struct s_philo	*next;
+	t_state			state;
 }					t_philo;
 
 
 // utils
 int valid_input(int ac, char **av);
 int	ft_atoi(char *nptr);
-
+t_philo	*create_philos(int size);
+void	free_philos(t_philo *head, size_t size);
+int	parsing(int ac, char **av, t_data *data);
+int	spread_forks(t_data *data);
 
 #endif
