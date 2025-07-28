385d0ef75dbe9ce88268ef98121d63f74c6703f4
4n4k1n <anakin@pregitzer.de>
2025-07-28 14:22:23 +0200

    created function ot destruct everything

---
+++ philo/src/utils/destruct.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destruct.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 11:47:21 by anakin            #+#    #+#             */
+/*   Updated: 2025/07/28 11:50:23 by anakin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/philo.h"
+
+void	desruct(t_data *data)
+{
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->death_mutex);
+	free_philos(data->philos, data->philo_amount);
+}
