6a1a0a5fff5e25a33a9c9951fbb63de7d77cc9cb
4n4k1n <anakin@pregitzer.de>
2025-08-05 11:53:17 +0200

    added delay for thinking

--- philo/src/jobs/think.c
+++ philo/src/jobs/think.c
@@ -14,8 +14,18 @@
 
 void	philo_think(t_philo *philo)
 {
+	long	think_time;
+
 	if (philo->data->simulation_end)
 		return ;
 	philo->state = THINK;
 	print_status(philo, "is thinking");
+	think_time = (philo->data->tt_die - (philo->data->tt_eat
+				+ philo->data->tt_sleep)) / 4;
+	if (think_time > 100)
+		think_time = 100;
+	else if (think_time < 0)
+		think_time = 0;
+	if (think_time > 0)
+		ft_usleep(think_time * 1000);
 }
