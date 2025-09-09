323fdbd5c71905e88b05a5bfd77d4bbe27faa612
4n4k1n <anakin@pregitzer.de>
2025-09-09 14:06:21 +0200

    changed the philo struct from a linked list to an normal array

--- philo/src/setup.c
+++ philo/src/setup.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 10:40:49 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/05 15:02:44 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/09 13:41:36 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,47 @@
 
 void init_philo(t_philo *philo, int id, t_data *data)
 {
-	philo->id = id;
-	philo->meals_eaten = 0;
-	philo->last_meal = 0;
-	philo->data = data;
-	philo->next = NULL;
+    philo->id = id;
+    philo->meals_eaten = 0;
+    philo->last_meal = 0;
+    philo->data = data;
 }
 
-int create_philo_linked_list(t_params *params, t_data *data)
+int create_philo_array(t_params *params, t_data *data)
 {
-	int i;
-	t_philo *current;
-	t_philo *previous;
+    int i;
 
-	i = 0;
-	previous = NULL;
-	while (i < params->num_philos)
-	{
-		current = malloc(sizeof(t_philo));
-		if (!current)
-			return (0);
-		init_philo(current, i + 1, data);
-		if (previous)
-		{
-			previous->next = current;
-			previous->right_fork = &current->left_fork;
-		}
-		else
-			data->philos = current;
-		previous = current;
-		i++;
-	}
-	return (1);
+    data->philos = malloc(sizeof(t_philo) * params->num_philos);
+    if (!data->philos)
+        return (0);
+    for (i = 0; i < params->num_philos; i++)
+    {
+        init_philo(&data->philos[i], i + 1, data);
+        if (i > 0)
+            data->philos[i - 1].right_fork = &data->philos[i].left_fork;
+    }
+    data->philos[params->num_philos - 1].right_fork = &data->philos[0].left_fork;
+    return (1);
 }
 
 int create_mutexes(t_data *data)
 {
-	t_philo *philo;
-	int i;
+    int i;
 
-	philo = data->philos;
-	i = 0;
-	while (i < data->params.num_philos)
-	{
-		if (pthread_mutex_init(&philo->left_fork, NULL) != 0)
-			return (0);
-		philo = philo->next;
-		i++;
-	}
-	return (1);
+    for (i = 0; i < data->params.num_philos; i++)
+    {
+        if (pthread_mutex_init(&data->philos[i].left_fork, NULL) != 0)
+            return (0);
+    }
+    return (1);
 }
 
 int setup(t_params *params, t_data *data)
 {
-	if (!create_philo_linked_list(params, data))
-		return (1);
-	data->params = *params;
-	data->dead = false;
-	data->start_time = get_time_in_ms();
-	return (0);
+    if (!create_philo_array(params, data))
+        return (1);
+    data->params = *params;
+    data->dead = false;
+    data->start_time = get_time_in_ms();
+    return (0);
 }
