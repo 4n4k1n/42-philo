7dcfb78040cd4ed0063f650437a959ff921f52a7
4n4k1n <anakin@pregitzer.de>
2025-09-10 15:08:17 +0200

    added function to print the philo status

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 14:37:03 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/05 14:37:19 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/10 14:54:52 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,18 @@ int get_time_in_ms(void)
 
 	gettimeofday(&tv, NULL);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+void	print_status(t_philo *philo, char *status)
+{
+	long long	timestamp;
+
+	pthread_mutex_lock(&philo->death_mutex);
+	if (philo->dead)
+		return ((void)pthread_mutex_unlock(&philo->death_mutex));
+	pthread_mutex_lock(&philo->print_mutex);
+	timestamp = get_time_in_ms() - philo->data->start_time;
+	printf("%lld %d %s\n", timestamp, philo->id, status);
+	pthread_mutex_unlock(&philo->print_mutex);
+	pthread_mutex_unlock(&philo->death_mutex);
 }
