a3c2c9178552bfb55f5c23acb75bf0dc3e20b79e
4n4k1n <anakin@pregitzer.de>
2025-09-12 14:29:11 +0200

    added modulo check if the philo id is odd or even and changed the grep strat depending on it.

--- philo/src/jobs.c
+++ philo/src/jobs.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 14:34:33 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/10 15:01:59 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/12 14:25:34 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 static int	ft_usleep(long time_in_ms, t_philo *philo)
 {
-	long start_time = get_time_in_ms();
+	long start_time;
 	long elapsed;
 
+	start_time = get_time_in_ms();
 	while (1)
 	{
 		elapsed = get_time_in_ms() - start_time;
-		if (elapsed >= time_in_ms || philo->dead)
+		if (philo->dead)
+			return (1);
+		if (elapsed >= time_in_ms)
 			break ;
 		usleep(100);
 	}
@@ -30,7 +33,8 @@ static int	ft_usleep(long time_in_ms, t_philo *philo)
 static int	sleeping(t_philo *philo)
 {
 	print_status(philo, "is sleeping");
-	ft_usleep(philo->data->params.time_to_sleep, philo);
+	if (ft_usleep(philo->data->params.time_to_sleep, philo))
+		return (1);
 	return (0);
 }
 
@@ -39,25 +43,50 @@ static int	thinking(t_philo *philo)
 	if (philo->dead)
 		return (1);
 	print_status(philo, "is thinking");
-	ft_usleep(philo->data->params.time_to_sleep, philo);
 	return (0);
+}
+
+static void	take_forks(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(&philo->left_fork);
+		print_status(philo, "has taken a fork");
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, "has taken a fork");
+	}
+	else
+	{
+		usleep(100);
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, "has taken a fork");
+		pthread_mutex_lock(&philo->left_fork);
+		print_status(philo, "has taken a fork");
+	}
 }
 
 static int	eating(t_philo *philo)
 {
+	int	exit_code;
+
+	exit_code = 0;
 	if (philo->dead)
 		return (1);
-	pthread_mutex_lock(&philo->left_fork);
-	print_status(philo, "has taken a fork");
-	pthread_mutex_lock(philo->right_fork);
-	print_status(philo, "has taken a fork");
+	take_forks(philo);
 	philo->last_meal = get_time_in_ms();
 	print_status(philo, "is eating");
-	ft_usleep(philo->data->params.time_to_eat, philo);
+	if (ft_usleep(philo->data->params.time_to_eat, philo))
+		exit_code = 1;
 	philo->meals_eaten++;
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(&philo->left_fork);
+		return (exit_code);
+	}
 	pthread_mutex_unlock(&philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
-	return (0);
+	return (exit_code);
 }
 
 void	*routine(void *arg)
@@ -65,10 +94,14 @@ void	*routine(void *arg)
 	t_philo *philo = (t_philo *)arg;
 
 	philo->last_meal = get_time_in_ms();
-	while (!philo->dead && (philo->data->params.num_meals == -1 || philo->meals_eaten < philo->data->params.num_meals))
+	while (!philo->dead)
 	{
-		if (eating(philo) || sleeping(philo) || thinking(philo))
-			break ; 
+		if (eating(philo))
+			break ;
+		if (sleeping(philo))
+			break ;
+		if (thinking(philo))
+			break ;
 	}
 	return (NULL);
 }	
