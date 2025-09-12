332b87f74a03a8a1a708ae4a208d0cbd3671fb9b
4n4k1n <anakin@pregitzer.de>
2025-09-12 14:43:30 +0200

    added break condition to the monitor function and changed for loops to while.

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 10:23:40 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/10 15:05:19 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/12 14:42:45 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int join_threads(t_data *data)
 {
 	int	i;
 
-	for (i = 0; i < data->params.num_philos; i++)
+	i = 0;
+	while (i < data->params.num_philos)
 	{
 		if (pthread_join(data->philos[i].thread, NULL) != 0)
 			return (0);
+		i++;
 	}
 	return (1);
 }
@@ -28,11 +30,13 @@ void	set_all_philos_dead(t_data *data)
 {
 	int	i;
 
-	for (i = 0; i < data->params.num_philos; i++)
+	i = 0;
+	while (i < data->params.num_philos)
 	{
 		pthread_mutex_lock(&data->philos[i].death_mutex);
 		data->philos[i].dead = true;
 		pthread_mutex_unlock(&data->philos[i].death_mutex);
+		i++;
 	}
 }
 
@@ -48,11 +52,13 @@ void monitor_philos(t_data *data)
 		{
 			current_time = get_time_in_ms();
 			pthread_mutex_lock(&data->philos[i].death_mutex);
-			if (!data->philos[i].dead && (current_time - data->philos[i].last_meal) > data->params.time_to_die)
+			if ((current_time - data->philos[i].last_meal) > data->params.time_to_die)
 			{
 				set_all_philos_dead(data);
+				pthread_mutex_unlock(&data->philos[i].death_mutex);
 				print_status(&data->philos[i], "died");
-				break;
+				i = -1;
+				return ;
 			}
 			pthread_mutex_unlock(&data->philos[i].death_mutex);
 			i++;
@@ -64,11 +70,12 @@ void	cleanup_data(t_data *data)
 {
 	int	i;
 
-	for (i = 0; i < data->params.num_philos; i++)
+	while (i < data->params.num_philos)
 	{
 		pthread_mutex_destroy(&data->philos[i].left_fork);
 		pthread_mutex_destroy(&data->philos[i].death_mutex);
 		pthread_mutex_destroy(&data->philos[i].print_mutex);
+		i++;
 	}
 	free(data->philos);
 }
@@ -76,6 +83,7 @@ void	cleanup_data(t_data *data)
 int	main(int argc, char **argv)
 {
 	t_data		data;
+
 	if (!parse_args(argc, argv, &data.params))
 		return (printf("Error: Invalid arguments\n"), 1);
 	setup(&data);
