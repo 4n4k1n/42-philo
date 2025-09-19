0c65b40ad6fecfa05e66cec78256512ed2b450ef
4n4k1n <anakin@pregitzer.de>
2025-09-19 16:00:20 +0200

    removed possible deadlock in print function

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 14:37:03 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/10 14:54:52 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/12 19:02:18 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,8 @@ void	print_status(t_philo *philo, char *status)
 {
 	long long	timestamp;
 
-	pthread_mutex_lock(&philo->death_mutex);
-	if (philo->dead)
-		return ((void)pthread_mutex_unlock(&philo->death_mutex));
 	pthread_mutex_lock(&philo->print_mutex);
 	timestamp = get_time_in_ms() - philo->data->start_time;
 	printf("%lld %d %s\n", timestamp, philo->id, status);
 	pthread_mutex_unlock(&philo->print_mutex);
-	pthread_mutex_unlock(&philo->death_mutex);
 }
