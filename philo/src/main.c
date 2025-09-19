3c6135311f1d583648e929ecc5315042202bb605
4n4k1n <anakin@pregitzer.de>
2025-09-19 16:01:27 +0200

    added debug prints

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 10:23:40 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/12 14:42:45 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/19 15:04:24 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int join_threads(t_data *data)
 	i = 0;
 	while (i < data->params.num_philos)
 	{
-		if (pthread_join(data->philos[i].thread, NULL) != 0)
+		if (pthread_create(&data->philos[i].thread, NULL, routine, &data->philos[i]) != 0)
 			return (0);
 		i++;
 	}
@@ -33,9 +33,7 @@ void	set_all_philos_dead(t_data *data)
 	i = 0;
 	while (i < data->params.num_philos)
 	{
-		pthread_mutex_lock(&data->philos[i].death_mutex);
 		data->philos[i].dead = true;
-		pthread_mutex_unlock(&data->philos[i].death_mutex);
 		i++;
 	}
 }
@@ -48,21 +46,23 @@ void monitor_philos(t_data *data)
 	while (true)
 	{
 		i = 0;
-		while (i < data->params.num_philos && !data->philos[i].dead)
+		while (i < data->params.num_philos)
 		{
-			current_time = get_time_in_ms();
 			pthread_mutex_lock(&data->philos[i].death_mutex);
+			current_time = get_time_in_ms();
+			// printf("%ld > %d\n", current_time - data->philos[i].last_meal, data->params.time_to_die);
 			if ((current_time - data->philos[i].last_meal) > data->params.time_to_die)
 			{
+				// printf("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 				set_all_philos_dead(data);
 				pthread_mutex_unlock(&data->philos[i].death_mutex);
 				print_status(&data->philos[i], "died");
-				i = -1;
 				return ;
 			}
 			pthread_mutex_unlock(&data->philos[i].death_mutex);
 			i++;
 		}
+		usleep(500);
 	}
 }
 
@@ -86,6 +86,8 @@ int	main(int argc, char **argv)
 
 	if (!parse_args(argc, argv, &data.params))
 		return (printf("Error: Invalid arguments\n"), 1);
+	// printf("tt_eat: %d\ntt_sleep %d\ntt_die %d\n", data.params.time_to_eat, data.params.time_to_sleep, data.params.time_to_die);
+	// return (0);
 	setup(&data);
 	join_threads(&data);
 	monitor_philos(&data);
