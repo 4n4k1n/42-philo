cb56b8f30408792b8ed665f9b6a62ac01567db5b
4n4k1n <anakin@pregitzer.de>
2025-09-20 04:13:21 +0200

    addeed function to interup if all philos ate enough

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 10:23:40 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/19 23:37:35 by anakin           ###   ########.fr       */
+/*   Updated: 2025/09/20 04:02:21 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,31 @@ void	set_all_philos_dead(t_data *data)
 	i = 0;
 	while (i < data->params.num_philos)
 	{
+		pthread_mutex_lock(&data->philos[i].death_mutex);
 		data->philos[i].dead = true;
+		pthread_mutex_unlock(&data->philos[i].death_mutex);
 		i++;
 	}
+}
+
+int check_for_enough_meals(t_data *data, int i)
+{
+	int			meals_eaten;
+	static int	check = -1;
+
+	if (data->params.num_meals == -1)	
+		return (0);
+	pthread_mutex_lock(&data->philos[i].meal_mutex);
+	meals_eaten = data->philos[i].meals_eaten;
+	pthread_mutex_unlock(&data->philos[i].meal_mutex);
+	if (meals_eaten < data->params.num_meals)
+	{
+		check = i;
+		return (0);
+	}
+	else if (check == i && meals_eaten >= data->params.num_meals)
+		return (1);
+	return (0);
 }
 
 void monitor_philos(t_data *data)
@@ -50,15 +72,17 @@ void monitor_philos(t_data *data)
 		while (i < data->params.num_philos)
 		{
 			current_time = get_time_in_ms();
-			pthread_mutex_lock(&data->philos[i].death_mutex);
+			pthread_mutex_lock(&data->philos[i].meal_mutex);
 			last_meal = data->philos[i].last_meal;
-			pthread_mutex_unlock(&data->philos[i].death_mutex);
+			pthread_mutex_unlock(&data->philos[i].meal_mutex);
 			if ((current_time - last_meal) > data->params.time_to_die)
 			{ 
 				set_all_philos_dead(data);
 				print_status(&data->philos[i], "died");
 				return ;
 			}
+			if (check_for_enough_meals(data, i))
+				return ((void)set_all_philos_dead(data));
 			i++;
 		}
 		usleep(500);
@@ -69,12 +93,13 @@ void	cleanup_data(t_data *data)
 {
 	int	i;
 
+	pthread_mutex_destroy(&data->print_mutex);
 	i = 0;
 	while (i < data->params.num_philos)
 	{
+		pthread_join(data->philos[i].thread, NULL);
 		pthread_mutex_destroy(&data->philos[i].left_fork);
 		pthread_mutex_destroy(&data->philos[i].death_mutex);
-		pthread_mutex_destroy(&data->philos[i].print_mutex);
 		i++;
 	}
 	free(data->philos);
@@ -85,11 +110,9 @@ int	main(int argc, char **argv)
 	t_data		data;
 
 	if (!parse_args(argc, argv, &data.params))
-		return (printf("Error: Invalid arguments\n"), 1);
-	// printf("tt_eat: %d\ntt_sleep %d\ntt_die %d\n", data.params.time_to_eat, data.params.time_to_sleep, data.params.time_to_die);
-	// return (0);
-	setup(&data);
-	// join_threads(&data);
+		return (1);
+	if (setup(&data))
+		return (printf("setup error\n"), 1);
 	monitor_philos(&data);
 	cleanup_data(&data);
 	return (0);
